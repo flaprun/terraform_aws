ede21c474c87a000468117bd1507889a9b5cf8db
Fabrice <fabrice.laprun@eiffage.com>
2023-05-30 15:05:29 +0000

    Single virtual server configured

--- main.tf
+++ main.tf
@@ -25,3 +25,27 @@ resource "aws_instance" "web-server" {
     Name = "web-server"
   }
 }
+
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "web-server-instance" {
+  ami               = data.aws_ami.ubuntu.id
+  instance_type     = "t2.micro"
+
+  user_data = "${file("install.sh")}"
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "web-server"
+  }
+
+}
